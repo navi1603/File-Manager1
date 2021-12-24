07536b9c6c50d9985d3504ad7fa4d8d842db2fd7
warlock <ukrainec1603@gmail.com>
2021-12-24 23:38:37 +0300

    UI

--- src/main/java/com/ukrainec/java/file/manager/Controller.java
+++ src/main/java/com/ukrainec/java/file/manager/Controller.java
@@ -1,4 +1,19 @@
 package com.ukrainec.java.file.manager;
 
-public class Controller {
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    public void btnExitActon(ActionEvent actionEvent) {
+        Platform.exit();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
