2fc662e6d60e4a912dea64a2164832d76fc1f1bc
warlock <ukrainec1603@gmail.com>
2021-12-26 00:06:47 +0300

    Add file Info

--- src/main/java/com/ukrainec/java/file/manager/FileInfo.java
+++ src/main/java/com/ukrainec/java/file/manager/FileInfo.java
@@ -1,12 +1,13 @@
 package com.ukrainec.java.file.manager;
 
-import sun.util.resources.LocaleData;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 public class FileInfo {
+
     public enum FileType {
         FILE("F"), DIRECTORY("D");
 
@@ -22,18 +23,53 @@ public class FileInfo {
     }
 
     private  String fileName;
-    private FileInfo type;
+    private FileType type;
     private long size;
-    private LocaleData lastModified;
+    private LocalDateTime lastModified;
 
-    public void setLastModified(LocaleData lastModified) {
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public FileType getType() {
+        return type;
+    }
+
+    public void setType(FileType type) {
+        this.type = type;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public LocalDateTime getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(LocalDateTime lastModified) {
         this.lastModified = lastModified;
     }
+
 
     public FileInfo (Path path) {
         try {
             this.fileName = path.getFileName().toString();
             this.size = Files.size(path);
+            this.type = Files.isDirectory(path) ? FileType.DIRECTORY : FileType.FILE;
+            if(this.type == FileType.DIRECTORY) {
+                this.size = -1L;
+            }
+            this.lastModified = LocalDateTime.ofInstant(Files.getLastModifiedTime(path).toInstant(), ZoneOffset.ofHours(0));
+
         } catch (IOException e) {
             throw new RuntimeException("Unable to create file info from path");
         }
